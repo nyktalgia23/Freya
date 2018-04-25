f64b6b809fc64844187c4ad0c317673c8245d297
Nyktalgia Thestarsaredeadnow <nyktalgia@weltmacht.local>
2018-04-26 01:41:11 +0200

    new commit

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -15,16 +15,16 @@
     class ArticleController extends AbstractController
     {
         /**
-         * @Route("/")
+         * @Route("/", name="app_homepage")
          */
         public function homepage()
         {
-            return new Response('OMG! My first page');
+            return $this->render('article/homepage.html.twig');
 
         }
 
         /**
-         * @Route("/news/{slug}")
+         * @Route("/news/{slug}", name="article_show")
          */
         public function show($slug)
         {
